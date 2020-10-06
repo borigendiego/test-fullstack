9063de9b443b8bfdfb46c037e91952693c8788d0
Diego Borigen <borigendiego@gmail.com>
2020-10-06 13:09:30 +1100

    Create react app + cleanning code + First component

---
+++ src/components/side-bar-menu/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//Style
+import './sideBarMenu.scss'
+
+const SideBarMenu = () => {
+    return (
+        <div className={'side-bar-container'}>
+        Side
+        </div>
+    )
+};
+
+export default SideBarMenu;
